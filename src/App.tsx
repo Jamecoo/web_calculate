8a081a5cd05a9565d5b8ff388165b1ee6b554eec
Jamecoo <thadsaphone9977@gmail.com>
2025-11-29 22:21:23 +0700

    Setup Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Box>
+        Calculate Web
+      </Box>
+    </>
+  )
+}
+
+export default App
---
+++ src/router/path.ts
@@ -0,0 +1,5 @@
+
+
+export const HOME_PATH: string = '/home';
+export const USER_PATH: string = '/home';
+export const REPORT_PATH: string = '/report';
